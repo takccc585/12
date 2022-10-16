9d5a818e22ca920f5d408dfb61f8fac2056de47b
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-10-16 11:16:06 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//boolean 23
+	/*int a, tus, sot, des, ed;
+	bool r;
+	cin >> a;
+	tus = a / 1000;
+	sot = (a % 1000) / 100;
+	des = ((a % 1000) % 100) / 10;
+	ed = ((a % 1000) % 100) % 10;
+	r = tus == ed and sot == des;
+	cout << boolalpha << r;*/
+	//boolean25
+	/*int x, y;
+	bool r;
+	cin >> x >> y;
+	r = x<0 and y>=0;
+	cout << boolalpha << r; */
+	//boolean30
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = a==b and b==c;
+	cout << boolalpha << r;*/
+	//boolean36
+	/*int x1, y1, x2, y2;
+	bool r;
+	cin >> x1 >> y1 >> x2 >> y2;
+	r = x1 == x2 and y1 == y2;
+	cout << boolalpha << r;*/
+	return 0;
+}
